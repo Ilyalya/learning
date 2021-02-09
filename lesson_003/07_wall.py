cd39fc23d2349af53ba89d0483c8eebb1ec02218
ilya <ilya.lebedev.98@mail.ru>
2021-02-09 17:27:33 +0300

    седьмое и восьмое не буду делать

--- lesson_003/07_wall.py
+++ lesson_003/07_wall.py
@@ -1,11 +1,16 @@
 # -*- coding: utf-8 -*-
 
 # (цикл for)
-import simple_draw
+import simple_draw as sd
 
 # Нарисовать стену из кирпичей. Размер кирпича - 100х50
 # Использовать вложенные циклы for
 
-# TODO здесь ваш код
+x = 0
+for i in range(10):
+    start_point = sd.get_point(50 + x, 50 + x)
+    end_point = sd.get_point(100 + x, 100 + x)
+    sd.rectangle(left_bottom=start_point, right_top=end_point, width=2)
+    x += 20
 
-simple_draw.pause()
+sd.pause()
